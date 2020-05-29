0d30adf23ed64b82e4ed28c54fa0573c6c377298
DJetzen <djetzen@users.noreply.github.com>
2020-05-29 16:24:44 +0200

    Add second part of second day

--- src/main/java/dje/advent/code/second/IntCodeCalculator.java
+++ src/main/java/dje/advent/code/second/IntCodeCalculator.java
@@ -1,35 +1,67 @@
 package dje.advent.code.second;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class IntCodeCalculator {
-    private List<Integer> parsedList;
 
-    public List<Integer> calculate(List<Integer> parsedList) {
-        for (int i = 0; i < parsedList.size() / 4; i++) {
-            int iterationOffset = i * 4;
-            int operationCode = parsedList.get(iterationOffset);
-            int firstPosition = parsedList.get(iterationOffset + 1);
-            int secondPosition = parsedList.get(iterationOffset + 2);
-            int sumPosition = parsedList.get(iterationOffset + 3);
+    private int solution;
+
+    public List<Integer> calculateFirstPart(List<Integer> parsedList) {
+        for (var i = 0; i < parsedList.size() / 4; i++) {
+            var iterationOffset = i * 4;
+            var operationCode = parsedList.get(iterationOffset);
             if (operationCode == 99) {
                 break;
             }
-            if (operationCode == 1) {
-                addValues(parsedList, firstPosition, secondPosition, sumPosition);
-            } else if (operationCode == 2) {
-                multiplyValues(parsedList, firstPosition, secondPosition, sumPosition);
-            }
+            doSingleStepCalculation(parsedList, iterationOffset, operationCode);
         }
         return parsedList;
     }
 
+    private void doSingleStepCalculation(List<Integer> parsedList, int iterationOffset, int operationCode) {
+        var firstPosition = parsedList.get(iterationOffset + 1);
+        var secondPosition = parsedList.get(iterationOffset + 2);
+        var sumPosition = parsedList.get(iterationOffset + 3);
+        if (operationCode == 1) {
+            addValues(parsedList, firstPosition, secondPosition, sumPosition);
+        } else if (operationCode == 2) {
+            multiplyValues(parsedList, firstPosition, secondPosition, sumPosition);
+        }
+    }
+
+    public void calculateSecondPart(List<Integer> parsedList) {
+        for (var noun = 0; noun <= 99; noun++) {
+            for (var verb = 0; verb <= 99; verb++) {
+                var parsedListCopy = new ArrayList<>(parsedList);
+                parsedListCopy.set(1, noun);
+                parsedListCopy.set(2, verb);
+                for (var i = 0; i < parsedList.size() / 4; i++) {
+                    var iterationOffset = i * 4;
+                    var operationCode = parsedListCopy.get(iterationOffset);
+                    doSingleStepCalculation(parsedListCopy, iterationOffset, operationCode);
+                    if (parsedListCopy.get(0) == 19690720) {
+                        calculateSolutionForSecondPart(parsedListCopy, noun, verb);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void calculateSolutionForSecondPart(List<Integer> parsedList, int noun, int verb) {
+        solution = (100 * noun + verb);
+    }
+
     private Integer multiplyValues(List<Integer> parsedList, int firstPosition, int secondPosition, int sumPosition) {
-        return parsedList.set(sumPosition, (parsedList.get(firstPosition) * parsedList.get(secondPosition)));
+        return parsedList.set(sumPosition, parsedList.get(firstPosition) * parsedList.get(secondPosition));
     }
 
     private Integer addValues(List<Integer> parsedList, int firstPosition, int secondPosition, int sumPosition) {
-        return parsedList.set(sumPosition, (parsedList.get(firstPosition) + parsedList.get(secondPosition)));
+        return parsedList.set(sumPosition, parsedList.get(firstPosition) + parsedList.get(secondPosition));
     }
 
 }
